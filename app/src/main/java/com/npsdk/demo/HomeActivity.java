58b27ac2ac2dbd3e59681efc99f0ed8b3b79ecc5
trongtrong <trongnguyenit1312@gmail.com>
2024-08-09 15:01:13 +0700

    Feat: remove query all pkg

--- app/src/main/java/com/npsdk/demo/HomeActivity.java
+++ app/src/main/java/com/npsdk/demo/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.npsdk.demo;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.text.method.HideReturnsTransformationMethod;
